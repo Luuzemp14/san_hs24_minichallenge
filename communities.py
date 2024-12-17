d2775a4ae4f250584322d0465ba74ab329ecba81
hariveliki <haris.alic@students.fhnw.ch>
2024-12-17 14:28:38 +0100

    Clean up

--- communities.py
+++ communities.py
@@ -3,3 +3,9 @@ import networkx as nx
 
 def get_girvan_newman_communities(G: nx.Graph):
     return list(nx.community.girvan_newman(G))
+
+def get_louvain_communities(G: nx.Graph):
+    return list(nx.community.louvain_communities(G))
+
+def get_label_propagation_communities(G: nx.Graph):
+    return list(nx.community.label_propagation_communities(G))
